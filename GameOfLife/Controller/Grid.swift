bd02acab59ce87067dff2d8b526f2e1cfb7cd670
Claudia Maciel <clc_80@yahoo.com>
2020-10-15 19:29:42 -0700

    populated grid with bubbles

--- GameOfLife/Controller/Grid.swift
+++ GameOfLife/Controller/Grid.swift
@@ -16,6 +16,9 @@ class Grid:SKSpriteNode {
     var cellWidth = 0
     var cellHeight = 0
     
+    // Creature
+    var gridArray = [[Creature]]()
+    
     // Called when a touch begins
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         // Only one touch
@@ -32,5 +35,47 @@ class Grid:SKSpriteNode {
         // Calculate individual cell dimensions
         cellWidth = Int(size.width) / columns
         cellHeight = Int(size.height) / rows
+        
+        //Populate grid
+        populateGrid()
     }
+
+
+    // Add a new creature at grid position
+    func addCreatureAtPosition(x: Int, y: Int) {
+        // New creature object
+        let creature = Creature()
+        
+        //Calculate position on screen
+        let gridPosition = CGPoint(x: x*cellWidth, y: y*cellWidth)
+        creature.position = gridPosition
+        
+        // set isAlive
+        creature.isAlive = true
+        
+        //Add to the grid
+        addChild(creature)
+        
+        // Add to the array
+        gridArray[x].append(creature)
+    }
+    
+    // Populate the grid with more creatures
+    func populateGrid() {
+        // Loop through the columns
+        for gridX in 0..<columns {
+            //Initialize empty column
+            gridArray.append([])
+            //Loop through rows
+            for gridY in 0..<rows {
+                //Create a new creature at row/column
+                addCreatureAtPosition(x: gridX, y: gridY)
+            }
+            
+        }
+    }
+    
+    
 }
+
+
