d0a27bc7677809de0582925aeb7435e0dbe19924
Claudia Maciel <clc_80@yahoo.com>
2020-10-15 18:32:23 -0700

    Added creatue

--- GameOfLife/Controller/Creature.swift
+++ GameOfLife/Controller/Creature.swift
@@ -8,5 +8,30 @@
 import SpriteKit
 
 class Creature: SKSpriteNode {
+    // Character
+    var isAlive: Bool = false {
+        didSet {
+            // Visibility
+            isHidden = !isAlive
+        }
+    }
     
+    // Living Neighbor Counter
+    var neighborCount = 0
+    
+    init() {
+        // Initialize with sphere
+        let texture = SKTexture(imageNamed: "BubbleOn")
+        super.init(texture: texture, color: UIColor.clear, size: texture.size())
+        
+        // Set the z position
+        zPosition = 1
+        
+        // Set anchor point to bottom - left instead of the middle
+        anchorPoint = CGPoint(x: 0, y: 0)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
 }
