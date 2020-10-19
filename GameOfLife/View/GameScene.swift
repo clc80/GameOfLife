e5887e760146084146ca119fdc36801cb9761cf8
Claudia Maciel <clc_80@yahoo.com>
2020-10-18 18:40:15 -0700

    Connected buttons and labels

--- GameOfLife/View/GameScene.swift
+++ GameOfLife/View/GameScene.swift
@@ -10,7 +10,25 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    // Objects
+    var stepForwardButton: ButtonNode!
+    var populationLabel: SKLabelNode!
+    var generationLabel: SKLabelNode!
+    var playButton: ButtonNode!
+    var pauseButton: ButtonNode!
+    var clearButton: ButtonNode!
+    var stopButton: ButtonNode!
+    
+    
     override func didMove(to view: SKView) {
+        // connect UI objects
+        stepForwardButton = childNode(withName: "stepOverButton") as! ButtonNode
+        populationLabel = childNode(withName: "populationLabel") as! SKLabelNode
+        generationLabel = childNode(withName: "generationLabel") as! SKLabelNode
+        playButton = childNode(withName: "playButton") as! ButtonNode
+        pauseButton = childNode(withName: "pauseButton") as! ButtonNode
+        clearButton = childNode(withName: "clearButton") as! ButtonNode
+        stopButton = childNode(withName: "stopButton") as! ButtonNode
         
     }
     
